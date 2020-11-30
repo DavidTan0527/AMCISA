966f80121f989f33361cd562b1c2bd2e958ed1b1
DavidTan0527 <david_weiucheng@hotmail.com>
2020-11-30 14:32:06 +0800

    Add fonts, illustrations; Start homepage

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// styles
+import '@/assets/scss/index.scss';
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="_home">
+    <nav class="navbar">
+      <section class="navbar-title">Amcisa</section>
+    </nav>
+    <div class="main-card">
+      <img class="svg" src="../assets/svg/having_fun.svg">
+      <div class="slogan-section">
+        <div class="slogan-1">
+          "Where no one
+        </div>
+        <div class="slogan-2">
+          is left behind"
+        </div>
+        <span class="quote">––Amcisa</span>
+
+        <div class="about-card">
+          <div class="info-section">
+            <header>About Us</header>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <img class="svg" src="../assets/svg/around_the_world.svg">
+        </div>
+        <div class="background-rect"></div>
+      </div>
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  data: () => ({
+  }),
 };
 </script>
