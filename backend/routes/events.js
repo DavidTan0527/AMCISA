497a39a526ac485ce01db758c9d0d0156ac499df
Wong Yung Jer <wongyungjer29@gmail.com>
2020-12-11 22:54:52 +0800

    nus_landing page update

---
+++ backend/routes/events.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var router = express.Router()
+
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+function getJson(filePath){
+    return readFile('./data/' + filePath);
+}
+
+router.get('/events', function (req, res) {
+    getJson('event.json').then( data =>{
+        data = JSON.parse(data).data;
+        data = data.map(({id, title, picture, event_date, venue}) => ({id, title, picture, event_date, venue}))
+        res.send(data)
+    })
+})
+
+router.get('/events/:id', function (req, res) {
+    getJson('event.json').then( data =>{
+        data = JSON.parse(data).data;
+        for (let i of data){
+            if (i.id == req.params.id){
+                res.send(i)
+            };
+            break;
+        }
+    })
+})
+
+
+
+module.exports = router
