7c02e68c808dd628d9ca96d7d65f52981abc4874
Tan Kel Zin <kelzzin2@gmail.com>
2020-12-24 22:31:16 +0800

    Update Data

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use('/',maincomm);
 app.use('/', contacts);
 app.use('/',foc);
 app.use('/',qna);
+app.use(express.static('/data/images'));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
