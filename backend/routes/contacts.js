f79830e42440b801ecb552b1243d996b89ec636a
Tan Kel Zin <kelzzin2@gmail.com>
2020-12-29 22:47:20 +0800

    Update API route

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -1,6 +1,6 @@
 const {router, getJson, addPrefix, param_rules, validationResult, fail} = require('./routes.js');
 
-router.get('/:uni/contacts', param_rules, function (req, res) {
+router.get('/:uni/contact', param_rules, function (req, res) {
     // If validation fails
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
