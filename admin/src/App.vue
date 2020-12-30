ad94580366e7c543ec7aa242758c1ca91fca1ce5
DavidTan0527 <david_weiucheng@hotmail.com>
2020-12-30 13:55:25 +0800

    Add feather icons and sidebar layout

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view v-if="$route.name === 'Login'"></router-view>
+    <layout v-else>
+      <router-view/>
+    </layout>
   </div>
 </template>
+
+<script>
+import layout from './layout/default.vue';
+
+export default {
+  components: {
+    layout,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100;300;400;500;700;900&display=swap');
--- admin/src/views/users.vue
+++ admin/src/views/users.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="_users">
-    This is the users page
+    <h1 class="title">Users <i class="fe fe-plus-circle"></i></h1>
   </div>
 </template>
 
@@ -12,3 +12,20 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+#_users {
+  .title {
+    display: flex;
+    align-items: center;
+    i {
+      margin-left: .8rem;
+      color: $gray-color;
+      cursor: pointer;
+      &:hover {
+        color: darken($gray-color, 8%);
+      }
+    }
+  }
+}
+</style>
