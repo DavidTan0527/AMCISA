1ee4707e5b10737bcdfcb9970921960dac4cbe15
DavidTan0527 <david_weiucheng@hotmail.com>
2021-01-12 15:22:56 +0800

    Change editor image prompt text

--- admin/src/components/editor/editor.vue
+++ admin/src/components/editor/editor.vue
@@ -243,7 +243,7 @@ export default {
     },
     showImagePrompt(command) {
       // eslint-disable-next-line
-      const src = prompt('Enter the url to your image (drag and drop if you are using images on your computer)');
+      const src = prompt('Enter the url to your image (drag and drop into the editor if you are using images on your computer)');
       if (src !== null) {
         command({ src });
       }
